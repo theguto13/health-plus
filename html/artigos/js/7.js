5b993b42fac11c66ebd9af467ee2a9eee16123c3
Augusto Roberto Tavares Junior <eogutin@outlook.com>
2022-04-10 16:48:44 -0300

    health-plus 2.1

--- html/artigos/js/7.js
+++ html/artigos/js/7.js
@@ -1,4 +1,5 @@
 window.onload = window.setTimeout('funcao()', 1*1000);
 function funcao(){
     document.getElementById("carregamento").style.display = "none";
+    document.getElementById("back7").style.animation = "back7";
 }
